1729f8033dffa46dbac5f305816158b89d10e9a5
christoph pirringer & ryan broughan <student@epicodus.com>
2015-09-28 11:07:57 -0700

    Add questions before displaying ticket form

--- app/routes/student.js
+++ app/routes/student.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  isFormShowing: true,
+  leads: ["Have you asked another pair for help?", "Have you spent 15 minutes going through through the problem documenting every step?"],
+
+  actions: {
+    nextLead: function() {
+      var leads = this.get('leads');
+
+      console.log(leads);
+      if (leads.length < 1) {
+        this.set('isFormShowing', true);
+      }
+      console.log(this.get('isFormShowing'));
+      $('.lead').text(leads.shift());
+    }
+  }
 });
