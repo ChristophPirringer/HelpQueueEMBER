8b937ae251565b54ad345c2362414e251032ab4f
chris pirringer & ryan broughan <student@epicodus.com>
2015-09-29 16:37:52 -0700

    refactor average wait time

--- app/components/statistics-tile.js
+++ app/components/statistics-tile.js
@@ -3,14 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   averageWaitTime: Ember.computed('day.tickets.@each.waitTime', function() {
   var sum = 0;
-  var helpedTickets = this.get('day.tickets').filterBy('helped', true);
+  var tickets = this.get('day.tickets');
+  var helpedTickets = tickets.filterBy('helped', true);
   var length = helpedTickets.length || 1;
-  for (var i = 0; i < this.get('day.tickets').get('length'); ++i) {
-    var waitTime = this.get('day.tickets').objectAt(i).get('waitTime');
-    if (!(isNaN(waitTime))) {
-      sum += waitTime;  // tho note to Ryan: some of your tickets do not have valid waitTime
+  
+  for (var i = 0; i < length; ++i) {
+    var waitTime = tickets.objectAt(i).get('waitTime');
+    if ( !(isNaN(waitTime)) ) {
+      sum += waitTime;
     }
   }
-  return Math.ceil(sum / (length)) + " minute(s)";
+  return Math.ceil(sum / length) + " minute(s)";
 })
 });
